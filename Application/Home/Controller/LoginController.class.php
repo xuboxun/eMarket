9abce59cd54669dd4284f826fc435d83fa7159b2
nicehmh <2896935608@qq.com>
2016-07-07 15:23:34 +0800

    sun

---
+++ Application/Home/Controller/LoginController.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class LoginController extends Controller {
+    public function index(){
+         $this->display();
+     }
+     public function Login(){
+          $this->display();
+     }
+       //
+     public function register(){
+        $this->display();
+     }
+     public function findpwd(){
+        $this->display();
+     }
+ }
